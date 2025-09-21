6324d58cf271e919233e23a252d98f444b9c57ca
Yusuf Ali <isaihmed09@gmail.com>
2025-09-21 09:49:29 +0600

    added some styles on property filter component

--- components/PropertyFilter.tsx
+++ components/PropertyFilter.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 function PropertyFilter() {
   return (
-      <section className="bg-neutral-800 py-10 md:py-16 -mt-20 relative z-[2] mx-auto max-w-6xl rounded-lg shadow-xl px-4 md:px-8">
+      <section className="bg-socendary py-10 md:py-16 -mt-20 relative z-[2] mx-auto max-w-6xl rounded-lg shadow-xl px-4 md:px-8">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
           <input
             type="text"
             placeholder="Enter Location"
-            className="p-3 bg-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
+            className="filter-inputs"
           />
           <select
-            className="p-3 bg-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
+            className="filter-inputs"
           >
             <option>Property Type</option>
             <option>Apartment</option>
@@ -18,7 +18,7 @@ function PropertyFilter() {
             <option>Condo</option>
           </select>
           <select
-            className="p-3 bg-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
+            className="filter-inputs"
           >
             <option>Price Range</option>
             <option>$100k - $200k</option>
@@ -28,7 +28,7 @@ function PropertyFilter() {
         </div>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-8">
           <select
-            className="p-3 bg-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
+            className="filter-inputs"
           >
             <option>Bedrooms</option>
             <option>1+</option>
@@ -36,7 +36,7 @@ function PropertyFilter() {
             <option>3+</option>
           </select>
           <select
-            className="p-3 bg-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
+            className="filter-inputs"
           >
             <option>Bathrooms</option>
             <option>1+</option>
@@ -44,13 +44,13 @@ function PropertyFilter() {
             <option>3+</option>
           </select>
           <select
-            className="p-3 bg-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
+            className="filter-inputs"
           >
             <option>For Sale</option>
             <option>For Rent</option>
           </select>
         </div>
-        <button className="w-full bg-green-500 text-white py-3 rounded-md text-lg font-semibold hover:bg-green-600 transition-colors">
+        <button className="w-full bg-action text-white py-3 rounded-md text-lg font-semibold hover:bg-green-800 transition-colors">
           Search
         </button>
       </section>
